b5ac4ff3e8d05255af6cedaa7b49325202fc1557
Debajyoti Halder <rondebajyoti@gmail.com>
2021-07-12 20:20:01 +0530

    chore: add backend template

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,12 +1,14 @@
+import {
+  getCLS, getFCP, getFID, getTTFB, getLCP,
+} from 'web-vitals';
+
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
